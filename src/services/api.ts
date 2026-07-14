8a9bbcdc387bc5912e2e5332b065d0a089b6bd6c
felix siu <felix.siu@metaverse-hk.cloud>
2026-07-14 11:06:14 +0800

    feat: cross-env

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,47 +1,47 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import axios from 'axios'
-import type { InternalAxiosRequestConfig, AxiosResponse } from 'axios'
+import axios from "axios";
+import type { InternalAxiosRequestConfig, AxiosResponse } from "axios";
 
-declare module 'axios' {
+declare module "axios" {
   export interface AxiosInstance {
-    get<T = any>(url: string, config?: AxiosRequestConfig): Promise<BaseVo<T>>
-    post<T = any>(url: string, data?: any, config?: AxiosRequestConfig): Promise<BaseVo<T>>
-    put<T = any>(url: string, data?: any, config?: AxiosRequestConfig): Promise<BaseVo<T>>
-    delete<T = any>(url: string, config?: AxiosRequestConfig): Promise<BaseVo<T>>
+    get<T = any>(url: string, config?: AxiosRequestConfig): Promise<BaseVo<T>>;
+    post<T = any>(url: string, data?: any, config?: AxiosRequestConfig): Promise<BaseVo<T>>;
+    put<T = any>(url: string, data?: any, config?: AxiosRequestConfig): Promise<BaseVo<T>>;
+    delete<T = any>(url: string, config?: AxiosRequestConfig): Promise<BaseVo<T>>;
   }
 }
 const api = axios.create({
   baseURL: `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/v1`,
   timeout: 10000,
   headers: {
-    'Content-Type': 'application/json'
-  }
-})
+    "Content-Type": "application/json",
+  },
+});
 
 api.interceptors.request.use(
   (config: InternalAxiosRequestConfig) => {
-    return config
+    return config;
   },
   (error) => {
-    return Promise.reject(error)
-  }
-)
+    return Promise.reject(error);
+  },
+);
 
 // Response 攔截器
 api.interceptors.response.use(
   (response: AxiosResponse) => {
-    return response.data as any
+    return response.data as any;
   },
   (error) => {
     if (error.response) {
       switch (error.response.status) {
         case 500:
-          console.error('💥 伺服器內部錯誤 (500)')
-          break
+          console.error("💥 伺服器內部錯誤 (500)");
+          break;
       }
     }
-    return Promise.reject(error)
-  }
-)
+    return Promise.reject(error);
+  },
+);
 
-export default api
+export default api;
